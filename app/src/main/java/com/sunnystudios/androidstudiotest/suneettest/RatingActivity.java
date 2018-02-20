3d2428cd58fc5d673f75f952953c43b0adc75349
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2018-02-20 15:36:25 +0000

    First commit of project files.

---
+++ app/src/main/java/com/sunnystudios/androidstudiotest/suneettest/RatingActivity.java
@@ -0,0 +1,32 @@
+package com.sunnystudios.androidstudiotest.suneettest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        WebView ratingWebview = (WebView) findViewById(R.id.rating_webview);
+        ratingWebview.loadUrl("https://play.google.com/store/apps/details?id=com.magicfortuneball.www");
+    }
+
+}
